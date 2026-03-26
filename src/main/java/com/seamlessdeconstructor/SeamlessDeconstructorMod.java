2e68ef9153b1c76bf2fbb1b0f9ccd16270ac5d9d
DerkOttersberg <derkottersberg@outlook.com>
2026-03-26 17:26:29 +0000

    Add Salvage Workbench to creative tab, rename to Salvage Workbench, add recipe advancement

--- src/main/java/com/seamlessdeconstructor/SeamlessDeconstructorMod.java
+++ src/main/java/com/seamlessdeconstructor/SeamlessDeconstructorMod.java
@@ -5,6 +5,8 @@ import com.seamlessdeconstructor.registry.ModBlockEntities;
 import com.seamlessdeconstructor.registry.ModBlocks;
 import com.seamlessdeconstructor.screen.ModScreenHandlers;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroups;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,6 +20,9 @@ public class SeamlessDeconstructorMod implements ModInitializer {
         ModBlocks.initialize();
         ModBlockEntities.initialize();
         ModScreenHandlers.initialize();
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(entries ->
+                entries.add(ModBlocks.REVERSE_DECONSTRUCTOR));
 
         LOGGER.info("Seamless Deconstructor initialized.");
     }
